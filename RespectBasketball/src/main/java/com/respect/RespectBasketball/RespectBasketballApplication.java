fad065163ba5be843eb8ae84037c722399c1b7eb
Fanny <fannyapriana@gmail.com>
2022-09-10 09:22:06 +0800

    First push

---
+++ RespectBasketball/src/main/java/com/respect/RespectBasketball/RespectBasketballApplication.java
@@ -0,0 +1,13 @@
+package com.respect.RespectBasketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespectBasketballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespectBasketballApplication.class, args);
+	}
+
+}
---
+++ RespectBasketball/src/test/java/com/respect/RespectBasketball/RespectBasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.respect.RespectBasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespectBasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
